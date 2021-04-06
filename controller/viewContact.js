d37a9c03e853821551891945f467d7e65899dd2e
teejohn247 <teejohn247@gmail.com>
2021-04-06 10:22:34 +0100

    fixed bugs

--- controller/viewContact.js
+++ controller/viewContact.js
@@ -6,14 +6,8 @@ const viewContact = async(req, res) => {
     // let limit = req.params.limit;
     try{
         const contacts = await Contact.find()
-        // .sort({date: -1})
-        // .limit(limit * 1)
-        // .skip((page - 1) * limit)
-        // .exec();
-        // console.log({adminrecords})
-
-        // const count = await Booking.find().countDocuments();
-        // console.log(count)
+         console.log(contacts[0])
+         let contact = contacts[0]
         if(!contacts){
             res.status(404).json({
                 status:404,
@@ -22,7 +16,8 @@ const viewContact = async(req, res) => {
         }
         res.status(200).json({
             status:200,
-            contacts,
+            contact
+            // contacts,
             // totalPages: Math.ceil(count / limit),
             // currentPage: page
         })
